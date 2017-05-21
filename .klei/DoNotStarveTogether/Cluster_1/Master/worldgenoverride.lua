c7e34f0906505bd085f3d06392e98a2bb0bbf395
Eibo Richter <eibo.richter@gmail.com>
2017-05-21 16:05:07 +0200

    added some additional "friendly" options

--- .klei/DoNotStarveTogether/Cluster_1/Master/worldgenoverride.lua
+++ .klei/DoNotStarveTogether/Cluster_1/Master/worldgenoverride.lua
@@ -3,33 +3,37 @@ override_enabled = true,
 preset = "SURVIVAL_TOGETHER", --  or "SURVIVAL_TOGETHER" or "SURVIVAL_TOGETHER_CLASSIC" or "SURVIVAL_DEFAULT_PLUS" or "COMPLETE_DARKNESS" or "DST_CAVE"    
     unprepared = { -- "never", "rare", "default", "often", "always"
         banana = "default",
-        berrybush = "default",
-        cactus = "default",
+        berrybush = "often",
+        cactus = "often",
         carrot = "default",
         lichen = "default",
-        mushroom = "default",    
+        mushroom = "often",    
     },    
+    
     misc = {
-        autumn = "default", -- "noseason", "veryshortseason", "shortseason", "default", "longseason", "verylongseason", "random"
+        autumn = "verylongseason", -- "noseason", "veryshortseason", "shortseason", "default", "longseason", "verylongseason", "random"
         boons = "default", -- "never", "rare", "default", "often", "always"
-        branching = "default", -- "never", "least", "default", "most"
+        branching = "never", -- "never", "least", "default", "most"
         cavelight = "default", -- "veryslow", "slow", "default", "fast", "veryfast"
-        day = "default", -- "default", "longday", "longdusk", "longnight", "noday", "nodusk", "nonight", "onlyday", "onlydusk", "onlynight"
+        day = "onlyday", -- "default", "longday", "longdusk", "longnight", "noday", "nodusk", "nonight", "onlyday", "onlydusk", "onlynight"
         earthquakes = "default", -- "never", "rare", "default", "often", "always"
         frograin = "default", -- "never", "rare", "default", "often", "always"
         lightning = "default", -- "never", "rare", "default", "often", "always"
-        loop = "default", -- "never", "default", "always"
+        loop = "always", -- "never", "default", "always"
         regrowth = "default", -- "veryslow", "slow", "default", "fast", "veryfast"
-        season_start = "default", -- "default", "winter", "spring", "summer", "autumnorspring", "winterorsummer", "random"
-        spring = "default", -- "noseason", "veryshortseason", "shortseason", "default", "longseason", "verylongseason", "random"
+        season_start = "autumn", -- "default", "winter", "spring", "summer", "autumnorspring", "winterorsummer", "random"
+        spring = "veryshortseason", -- "noseason", "veryshortseason", "shortseason", "default", "longseason", "verylongseason", "random"
         start_location = "default", -- "caves", "default", "plus", "darkness"
-        summer = "default", -- "noseason", "veryshortseason", "shortseason", "default", "longseason", "verylongseason", "random"
+        summer = "noseason", -- "noseason", "veryshortseason", "shortseason", "default", "longseason", "verylongseason", "random"
         task_set = "default", -- "classic", "default", "cave_default"
         touchstone = "default", -- "never", "rare", "default", "often", "always"
         weather = "default", -- "never", "rare", "default", "often", "always"
         wildfires = "default", -- "never", "rare", "default", "often", "always"
-        winter = "default", -- "noseason", "veryshortseason", "shortseason", "default", "longseason", "verylongseason", "random"
-        world_size = "default", -- "small", "medium", "default", "huge"    },    animals = { -- "never", "rare", "default", "often", "always"
+        winter = "noseason", -- "noseason", "veryshortseason", "shortseason", "default", "longseason", "verylongseason", "random"
+        world_size = "small", -- "small", "medium", "default", "huge"
+    },    
+    
+    animals = { -- "never", "rare", "default", "often", "always"
         alternatehunt = "default",
         angrybees = "default",
         beefalo = "default",    
@@ -45,7 +49,7 @@ preset = "SURVIVAL_TOGETHER", --  or "SURVIVAL_TOGETHER" or "SURVIVAL_TOGETHER_C
         lightninggoat = "default",
         moles = "default",
         monkey = "default",
-        penguins = "default",
+        penguins = "never",
         perd = "default",
         pigs = "default",
         rabbits = "default",
@@ -70,28 +74,28 @@ preset = "SURVIVAL_TOGETHER", --  or "SURVIVAL_TOGETHER" or "SURVIVAL_TOGETHER_C
         liefs = "default",
         lureplants = "default",
         merm = "default",
-        spiders = "default",
+        spiders = "often",
         tentacles = "default",
         walrus = "default",
         worms = "default",    
     },    
 
     resources = { -- "never", "rare", "default", "often", "always"
-        fern = "default",
-        flint = "default",
+        fern = "often",
+        flint = "often",
         flower_cave = "default",
-        flowers = "default",
-        grass = "default",
+        flowers = "often",
+        grass = "often",
         marshbush = "default",
         meteorshowers = "default",
         meteorspawner = "default",
         mushtree = "default",
         reeds = "default",
-        rock = "default",
-        rock_ice = "default",
-        sapling = "default",
+        rock = "often",
+        rock_ice = "often",
+        sapling = "often",
         trees = "default",
         tumbleweed = "default",
         wormlights = "default",    
-    },    
+    },
 }
