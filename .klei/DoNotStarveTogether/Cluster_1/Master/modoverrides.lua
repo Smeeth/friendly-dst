20fc428157e4e132ee5a1baaf2ed20e46ddf529f
Eibo Richter <eibo.richter@gmail.com>
2017-05-21 17:50:03 +0200

    enabled those nice mods

---
+++ .klei/DoNotStarveTogether/Cluster_1/Master/modoverrides.lua
@@ -0,0 +1,9 @@
+return {
+--#Global Position
+["workshop-378160973"] = { enabled = true },
+
+--#Display Food Values
+["347079953"] = { enabled = true },
+
+--#Health Info
+["375859599"] = { enabled = true },
